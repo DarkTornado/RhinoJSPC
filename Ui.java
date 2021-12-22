7463964b3ac3cd0439fdb36174995aebb53a46d5
DarkTornado <dt3141592@naver.com>
2021-12-22 23:58:46 +0900

    몰?루

--- Ui.java
+++ Ui.java
@@ -2,6 +2,9 @@ import java.awt.Dimension;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import java.awt.GridLayout;
 import java.awt.Color;
 
@@ -10,6 +13,15 @@ public class Ui {
     public static void main(String[] args) {
         JFrame window = new JFrame("Test Window");
         window.setLayout(new GridLayout(1, 2, 10, 10));
+
+        JMenuBar bar = new JMenuBar();
+        JMenu menu = new JMenu("Hello");
+        JMenuItem item = new JMenuItem("World");
+        menu.add(item);
+        bar.add(menu);
+        
+        window.setJMenuBar(bar);
+
 
         JTextArea input = new JTextArea();
         input.setBorder(BorderFactory.createLineBorder(Color.BLACK));
