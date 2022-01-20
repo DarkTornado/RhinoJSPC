dadb56d77f53011a4a6de382b2442c3e235b4f00
Dark Tornado <dt3141592@naver.com>
2022-01-20 14:18:14 +0900

    일단 실행하는거 성공

--- src/com/darktornado/rhinojspc/MainUI.java
+++ src/com/darktornado/rhinojspc/MainUI.java
@@ -3,6 +3,11 @@ package com.darktornado.rhinojspc;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ImporterTopLevel;
+import org.mozilla.javascript.ScriptableObject;
+
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -14,18 +19,18 @@ import java.awt.Color;
 public class MainUI {
 
     public static void main(String[] args) {
-    	new MainUI().onCreate();
+        new MainUI().onCreate();
     }
-    
+
     private JMenuBar createTitleBar(final JTextArea input, final JTextArea output) {
         JMenuBar title = new JMenuBar();
 
         JMenu menu1 = new JMenu("File");
         JMenuItem item11 = new JMenuItem("Exit");
         item11.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		System.exit(0);
-        	}
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
         });
         menu1.add(item11);
         title.add(menu1);
@@ -33,18 +38,19 @@ public class MainUI {
         JMenu menu2 = new JMenu("Run");
         JMenuItem item21 = new JMenuItem("Run Source");
         item21.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		String src = input.getText();
-        		System.out.println(src);
-        	}
+            public void actionPerformed(ActionEvent e) {
+                String src = input.getText();
+                String result = runScript(src);
+                output.setText(result);
+            }
         });
         menu2.add(item21);
         title.add(menu2);
 
         return title;
-    	
+
     }
-    
+
     private void onCreate() {
         JFrame window = new JFrame("JS Executor via Rhino");
         window.setLayout(new GridLayout(1, 2, 10, 10));
@@ -58,12 +64,27 @@ public class MainUI {
 
         JMenuBar title = createTitleBar(input, output);
         window.setJMenuBar(title);
-        
+
         window.setExtendedState(JFrame.MAXIMIZED_BOTH);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setSize(400, 300);
         window.setVisible(true);
     }
-    
+
+    private String runScript(String src) {
+        try {
+            Context rhino = Context.enter();
+            rhino.setOptimizationLevel(-1);
+            rhino.setLanguageVersion(Context.VERSION_ES6);
+            ScriptableObject scope = new ImporterTopLevel(rhino);
+            String result = String.valueOf(rhino.evaluateString(scope, src, "JavaScript", 1, null));
+            Context.exit();
+            return result;
+        } catch (Exception e) {            
+            Context.exit();
+            return e.toString();
+        }
+    }
+
 
 }
